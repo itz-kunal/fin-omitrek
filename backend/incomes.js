83bd012f2ba6020c8505ad3e9b80a6b5b20e8f5b
Kunal Kumar <123314137+itz-kunal@users.noreply.github.com>
2023-12-11 19:51:07 +0530

    Update incomes.js

--- backend/incomes.js
+++ backend/incomes.js
@@ -7,16 +7,45 @@ async function updRefIncome(data, couponWorth) {
         const userReferrals = await User.find({
             'referrals._id': userIdToFind
         });
-        userReferrals.forEach(async (i) => {
-            const refIndex = i.referrals.findIndex(referral => referral._id == userIdToFind);
-            const level = i.referrals[refIndex].level;
+        const User = require('./schemas.js');
+
+async function updRefIncome(data, couponWorth) {
+    try {
+        const userIdToFind = data._id;
+        // Find all users who contain the referral
+        const userReferrals = await User.find({
+            'referrals._id': userIdToFind
+        });
+
+        for (const user of userReferrals) {
+            const refIndex = user.referrals.findIndex(referral => referral._id == userIdToFind);
+            const level = user.referrals[refIndex].level;
 
             // Calculate earnings based on referral level
-            const earningPercentage = getEarningPercentage(level);
-            i.earning = i.earning + couponWorth * earningPercentage / 100 || 0;
-        });
-        // Save all the users in one go
-        await Promise.all(userReferrals.map(user => user.save()));
+            const earningPercentage = await getEarningPercentage(level);
+            user.earning = user.earning + couponWorth * earningPercentage / 100 || 0;
+
+            // Save each user individually
+            await user.save();
+        }
+    } catch (err) {
+        throw err;
+    }
+}
+
+function getEarningPercentage(level) {
+    const percentageMap = {
+        1: 10,
+        2: 5,
+        3: 3,
+        4: 2,
+        5: 1
+    };
+    return percentageMap[level] || 0;
+}
+
+module.exports = updRefIncome;
+
     } catch (err) {
         throw err;
     }
